0968ed93ce69b8ac361b8e0434fb0f4735c8a453
Himanshugarg0007 <hunnygarg.bboy@gmail.com>
2023-09-20 14:38:34 +0530

    done

--- unit_2/masai_onboarding/src/oddEven.js
+++ unit_2/masai_onboarding/src/oddEven.js
@@ -1,13 +1,24 @@
 function generateNumber() {
   // generate a random integer(hint : Math.random)
+  let numDiv = document.getElementById("number")
+  let randomnum =  Math.floor(Math.random() * 100) +1;
+  numDiv.textContent = randomnum;
+  checkOddEven(randomnum);
 }
 
 function checkOddEven(num) {
   // logic for even / odd
+  let oddeve = document.getElementById("odd-even")
+  if(num%2==0) oddeve.textContent = "The number is even"
+  else oddeve.textContent = "The number is odd";
 }
 
 window.onload = function () {
   // add event listeners to the button here
+  let btn = document.getElementById("generate-number");
+  btn.addEventListener("click", function() {
+    generateNumber();
+  })
 };
 
 // donot change the following export statement
