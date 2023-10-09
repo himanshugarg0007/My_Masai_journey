84003675ef85ccd39901de826cd5d9ed063b1854
Himanshugarg0007 <hunnygarg.bboy@gmail.com>
2023-10-09 13:15:14 +0530

    custom Array done

--- unit_2/ES6_II_Classes/index.js
+++ unit_2/ES6_II_Classes/index.js
@@ -1,16 +1,50 @@
-let main = document.getElementById("root");
-main.textContent = "hello world"
+// let main = document.getElementById("root");
+// main.textContent = "hello world"
 
+class custom_array {
+  constructor(...args) {
+    this.size = args.length;
+    for (let i = 0; i < args.length; i++) {
+      this[i] = args[i];
+    }
+  }
 
-class custom_array{
-    constructor (arr){
-        return arr;
+  push(ele) {
+    this[this.size] = ele;
+    this.size++;
+    return this.print()
+  }
+  pop() {
+    delete this[this.size - 1];
+    this.size--;
+    console.log(this.print())
+    return this[this.size - 1];
+  }
+  top() {
+    return this[this.size - 1];
+  }
+
+  print() {
+    let arr = [];
+    for(let i in this){
+        if(i=="size"){
+            continue;
+        }
+        else arr.push(this[i]);
     }
-    Push(ele){
-        
-    }
+    return arr;
+  }
+
+  printReverse() {
+    return this.print().reverse()
+  }
+
+  getSize(){
+    return this.size;
+  }
 }
 
-let arr1 = new custom_array([1,2,3,4,5,5]);
+let arr1 = new custom_array(1, 2, 3, 4);
+console.log(arr1.getSize());
 
-console.log(arr1);
+
